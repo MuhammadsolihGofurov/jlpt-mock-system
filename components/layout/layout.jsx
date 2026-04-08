6988a782a81377d40f31de364fd629dbae6e7da4
MuhammadsolihGofurov <gofurovmuhammadsolih004@gmail.com>
2026-04-08 14:24:47 +0500

    changes: google analytics are added

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -92,6 +92,22 @@ const Layout = ({ children }) => {
 
 
         <meta name="google-site-verification" content="nkQkcYk98918n_BSnRwf4Qgz3EuBf-ZI0WI4ts6RS1w" />
+
+
+        {/* Google Analytics Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RFDSBP8BPG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-RFDSBP8BPG');
+          `}
+        </Script>
       </Head>
 
       {hasSidebar || router.pathname === "/" ? (
